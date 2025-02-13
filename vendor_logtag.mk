9db5578aec8d7895da3eea307255077734795468
Anaël <60509176+Bakoubak@users.noreply.github.com>
2025-02-13 02:35:48 +0100

    logcat: mute logspam while playing videos

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -269,4 +269,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.MtkCam/P1NodeUtility=$(VENDOR_LOG_LEVEL_I) \
     persist.log.tag.TuningMgr=$(VENDOR_LOG_LEVEL_I) \
     persist.log.tag.Camera3-OutputUtils=$(VENDOR_LOG_LEVEL_I) \
-    persist.log.tag.ULog=$(VENDOR_LOG_LEVEL_I)
+    persist.log.tag.ULog=$(VENDOR_LOG_LEVEL_I) \
+    persist.log.tag.VDO_LOG=$(VENDOR_LOG_LEVEL_I)
