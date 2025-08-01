c2a6f9bd9472f63be21a0390b586617f50b7d38c
Mathieu Chartier <mathieuc@google.com>
2025-08-01 13:46:41 +0200

    mt6765-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- mt6765.mk
+++ mt6765.mk
@@ -340,6 +340,9 @@ PRODUCT_PACKAGES += \
     libshim_showlogo \
     libshim_vtservice
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Text classifier
 PRODUCT_PACKAGES += \
     libtextclassifier_hash \
