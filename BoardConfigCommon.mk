4298e74f1ae13bb0e60ab46306774e1a7aeeffaf
VistaSlayer <Bakoubak85@gmail.com>
2025-11-25 23:33:56 +0100

    vintf: add FCM
    * Fixes a bunch of missing declarations

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,7 +37,8 @@ DEVICE_MATRIX_FILE += $(COMMON_PATH)/vintf/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml \
+    $(COMMON_PATH)/vintf/framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/manifest.xml
 
