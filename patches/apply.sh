927a9c1c41c4f6e4b6a23349c9076aaaaf860759
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-01-05 00:16:35 +0100

    patches: update patches

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd vendor/lineage
-patch -p1 < ../../device/lenovo/mt6765-common/patches/vendor/lineage/0001-vendor-lineage-build-soong-Let-the-user-build-withou.patch
+patch -p1 < ../../device/lenovo/mt6765-common/patches/vendor/lineage/0001-soong-Permit-building-with-a-prebuilt-kernel.patch
 cd ../..
 cd packages/modules/Connectivity
 patch -p1 < ../../../device/lenovo/mt6765-common/patches/packages/modules/Connectivity/0001-connectivity-clat_coordinator-Disable-SELinux-contex.patch
