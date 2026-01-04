e8168376500d49d651dc87686cf0e69a30dc6549
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-01-04 23:33:37 +0100

    patches: Update patches for LineageOS 21

--- patches/apply.sh
+++ patches/apply.sh
@@ -2,3 +2,5 @@
 cd vendor/lineage
 patch -p1 < ../../device/lenovo/mt6765-common/patches/vendor/lineage/0001-vendor-lineage-build-soong-Let-the-user-build-withou.patch
 cd ../..
+cd packages/modules/Connectivity
+patch -p1 < ../../../device/lenovo/mt6765-common/patches/packages/modules/Connectivity/0001-connectivity-clat_coordinator-Disable-SELinux-contex.patch
