6f98ec6d3209df62f458ed8fdf0ec21f50cff736
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-02-15 02:00:08 +0100

    mt6765: build mtkpower from source

--- mt6765.mk
+++ mt6765.mk
@@ -272,6 +272,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor \
     android.hardware.power@1.3.vendor \
+    vendor.mediatek.hardware.mtkpower@1.0.vendor \
     libexpat.vendor \
     libpower.vendor
 
