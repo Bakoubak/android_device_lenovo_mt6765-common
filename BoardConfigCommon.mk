d3dedd07962ae1bfa3acdca1b4bf733c32a3069f
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-03-19 18:13:40 +0100

    board: avb: set vbmeta flags to 3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,7 +104,7 @@ WIFI_DRIVER_STATE_ON := "1"
 # Verified Boot
 BOARD_AVB_ENABLE := true
 
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 BOARD_AVB_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
@@ -120,9 +120,6 @@ BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 0
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
-
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += \
-    --chain_partition recovery:1:external/avb/test/data/testkey_rsa2048.pem
 
 BOARD_AVB_VBMETA_SYSTEM := system
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
