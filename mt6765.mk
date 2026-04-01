fdd72779017391c2c07dc7bf3ee9b6a7fc0694e5
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-04-01 20:54:36 +0200

    mt6765: test things on the powerhal

--- mt6765.mk
+++ mt6765.mk
@@ -271,7 +271,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-mediatek \
+    android.hardware.power-service.lineage-libperfmgr \
+    vendor.mediatek.hardware.mtkpower@1.2-service.stub \
+    libmtkperf_client_vendor \
+    libmtkperf_client
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1.vendor \
     android.hardware.power@1.2.vendor \
