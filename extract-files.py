8e42816fe74be813a468e05d18f95a8363dcca7f
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-04-08 17:59:32 +0200

    extract-files.py: fix typo

--- extract-files.py
+++ extract-files.py
@@ -121,10 +121,10 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('__gnu_Unwind_Find_exidx'),
 
     ('vendor/lib/libwifi-hal-mtk.so', 'vendor/lib64/libwifi-hal-mtk.so'): blob_fixup()
-        .set_soname('libwifi-hal-mtk.so'),
+        .fix_soname('libwifi-hal-mtk.so'),
 
     ('vendor/lib/libalsautils-v29.so', 'vendor/lib64/libalsautils-v29.so'): blob_fixup()
-        .set_soname('libalsautils-v29.so'),
+        .fix_soname('libalsautils-v29.so'),
 }
 
 lib_fixups: lib_fixups_user_type = {
