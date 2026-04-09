e76726b99e4302c72d06ae2f4c77744e4d4dab04
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-04-09 13:59:47 +0200

    board: enable BUILD_BROKEN_PREBUILT_ELF_FILES
    * The dependency issues will be fixed when the build will be complete

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,6 +40,7 @@ $(call soong_config_set,lineage_health,charging_control_supports_deadline,false)
 # Broken
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_PREBUILT_ELF_FILES := true
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := mt6765
