bb69a6e6ef8f7df55e16b61e85b8d9ea989be3ab
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-04-09 23:46:40 +0200

    mt6765: disable A/B explicitly
    * File "ota_from_target_files.py", line 952, in GenerateAbOtaPackage
    AssertionError: META/ab_partitions.txt is required for ab_update.

--- mt6765.mk
+++ mt6765.mk
@@ -7,6 +7,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Disable A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
