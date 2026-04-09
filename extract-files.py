7a58a37d097ae28b5903e1171d8646cbc9adff3a
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-04-09 13:07:32 +0200

    proprietary: patch libutinterface_custom_md to use libutinterface_md

--- extract-files.py
+++ extract-files.py
@@ -93,6 +93,9 @@ blob_fixups: blob_fixups_user_type = {
     
     ('vendor/lib/libdlbdsservice.so', 'vendor/lib64/libdlbdsservice.so', 'vendor/lib/soundfx/libswdap.so', 'vendor/lib64/soundfx/libswdap.so'): blob_fixup()
         .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
+
+    ('vendor/lib/libutinterface_custom_md.so', 'vendor/lib64/libutinterface_custom_md.so'): blob_fixup()
+        .add_needed('libutinterface_md.so'),
     
     'vendor/lib/libvcodec_oal.so': blob_fixup()
         .clear_symbol_version('__aeabi_memcpy')
