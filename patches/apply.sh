262f97fc063d9409362316d95524e04bf2dcd73d
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-05-18 20:02:28 +0200

    patches: update patches for Android 16

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd vendor/lineage
-patch -p1 < ../../device/lenovo/mt6765-common/patches/vendor/lineage/0001-soong-Permit-building-with-a-prebuilt-kernel.patch
+patch -p1 < ../../device/lenovo/mt6765-common/patches/vendor/lineage/0001-PATCH-Permit-building-with-a-prebuilt-kernel.patch
 cd ../..
 cd packages/modules/Connectivity
 patch -p1 < ../../../device/lenovo/mt6765-common/patches/packages/modules/Connectivity/0001-connectivity-clat_coordinator-Disable-SELinux-contex.patch
