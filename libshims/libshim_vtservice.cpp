392940985449dccdb3033411f9055ba432f31835
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-06-01 23:18:19 +0200

    libshims: fix vtservice shim for Android 16

--- libshims/libshim_vtservice.cpp
+++ libshims/libshim_vtservice.cpp
@@ -1,6 +1,8 @@
 #include <media/AudioTrack.h>
 #include <gui/IGraphicBufferProducer.h>
 #include <media/stagefright/MediaMuxer.h>
+#include <map>
+#include <mutex>
 
 using namespace android;
 
@@ -60,12 +62,11 @@ extern "C" {
             size_t frameCount,
             audio_output_flags_t flags,
             const wp<android::AudioTrack::IAudioTrackCallback>& callbackWeak,
-            void* user,
             int32_t notificationFrames,
             audio_session_t sessionId,
             android::AudioTrack::transfer_type transferType,
             const audio_offload_info_t *offloadInfo,
-            const AttributionSourceState& attributionSource,
+            const android::content::AttributionSourceState& attributionSource,
             const audio_attributes_t* pAttributes,
             bool doNotReconnect,
             float maxRequiredSpeed,
@@ -92,49 +93,50 @@ extern "C" {
             float maxRequiredSpeed,
             audio_port_handle_t selectedDeviceId) {
 
-	android::content::pm::AttributionSourceState attributionSource;
+        android::content::AttributionSourceState attributionSource;
         attributionSource.uid = uid;
         attributionSource.pid = pid;
         attributionSource.token = sp<BBinder>::make();
 
-	sp<ShimLegacyCallbackWrapper> modernCallback = nullptr;
+        sp<ShimLegacyCallbackWrapper> modernCallback = nullptr;
         if (cbf) {
             modernCallback = sp<ShimLegacyCallbackWrapper>::make(cbf, user);
             
-            // Verrouiller et sauvegarder le sp<> pour empêcher sa destruction automatique
             std::lock_guard<std::mutex> lock(gCallbackLockMutex);
             gCallbackLockMap[thisptr] = modernCallback;
         }
 
-	android::wp<android::AudioTrack::IAudioTrackCallback> callbackWeak(modernCallback);
+        wp<android::AudioTrack::IAudioTrackCallback> callbackWeak(modernCallback);
 
         _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tj20audio_output_flags_tRKNS_2wpINS0_19IAudioTrackCallbackEEEi15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi(
-                            thisptr, streamType, sampleRate, format, channelMask, frameCount, flags, callbackWeak,
-                            user, notificationFrames, sessionId, transferType, offloadInfo,
-                            *attributionSource, pAttributes,
-                            doNotReconnect, maxRequiredSpeed, selectedDeviceId);
+                     thisptr, streamType, sampleRate, format, channelMask, frameCount, flags, callbackWeak,
+                     notificationFrames, sessionId, transferType, offloadInfo,
+                     attributionSource, pAttributes,
+                     doNotReconnect, maxRequiredSpeed, selectedDeviceId);
+    }
+
+    void _ZN7android10AudioTrackD1Ev(void* thisptr) {
+        std::lock_guard<std::mutex> lock(gCallbackLockMutex);
+        gCallbackLockMap.erase(thisptr);
     }
 
     void _ZN7android11AudioSourceC1EPK18audio_attributes_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf(const audio_attributes_t *attr, const String16 &opPackageName, uint32_t sampleRate, uint32_t channelCount, uint32_t outSampleRate, uid_t uid, pid_t pid, audio_port_handle_t selectedDeviceId, audio_microphone_direction_t selectedMicDirection, float selectedMicFieldDimension);
 
-   void _ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf(__attribute__((unused)) audio_source_t inputSource, const String16 &opPackageName, uint32_t sampleRate, uint32_t channelCount, uint32_t outSampleRate, uid_t uid, pid_t pid, audio_port_handle_t selectedDeviceId, audio_microphone_direction_t selectedMicDirection, float selectedMicFieldDimension) {
-        audio_attributes_t attr = AUDIO_ATTRIBUTES_INITIALIZER;
-        attr.source = AUDIO_SOURCE_DEFAULT;
-        _ZN7android11AudioSourceC1EPK18audio_attributes_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf(&attr, opPackageName, sampleRate, channelCount, outSampleRate, uid, pid, selectedDeviceId, selectedMicDirection, selectedMicFieldDimension);
+    void _ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf(__attribute__((unused)) audio_source_t inputSource, const String16 &opPackageName, uint32_t sampleRate, uint32_t channelCount, uint32_t outSampleRate, uid_t uid, pid_t pid, audio_port_handle_t selectedDeviceId, audio_microphone_direction_t selectedMicDirection, float selectedMicFieldDimension) {
+         audio_attributes_t attr = AUDIO_ATTRIBUTES_INITIALIZER;
+         attr.source = AUDIO_SOURCE_DEFAULT;
+         _ZN7android11AudioSourceC1EPK18audio_attributes_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf(&attr, opPackageName, sampleRate, channelCount, outSampleRate, uid, pid, selectedDeviceId, selectedMicDirection, selectedMicFieldDimension);
     }
 
-    void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
-        void* thisptr, const sp<IGraphicBufferProducer>& bufferProducer, bool controlledByApp, const sp<IBinder>& surfaceControlHandle);
+    void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(void* thisptr, const sp<IGraphicBufferProducer>& bufferProducer, bool controlledByApp, const sp<IBinder>& surfaceControlHandle);
 
-    void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb(
-        void* thisptr, const sp<IGraphicBufferProducer> &bufferProducer, bool controlledByApp) {
-            _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(thisptr, bufferProducer, controlledByApp, nullptr);
+    void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb(void* thisptr, const sp<IGraphicBufferProducer> &bufferProducer, bool controlledByApp) {
+         _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(thisptr, bufferProducer, controlledByApp, nullptr);
     }
 
     void _ZN7android10MediaMuxerC1EiNS_14MediaMuxerBase12OutputFormatE(void* thisptr, int fd, android::MediaMuxer::OutputFormat format);
 
     void _ZN7android10MediaMuxerC1EiNS0_12OutputFormatE(void* thisptr, int fd, android::MediaMuxer::OutputFormat format) {
-            _ZN7android10MediaMuxerC1EiNS_14MediaMuxerBase12OutputFormatE(thisptr, fd, format);
+         _ZN7android10MediaMuxerC1EiNS_14MediaMuxerBase12OutputFormatE(thisptr, fd, format);
     }
-    gCallbackLockMap.erase(thisptr);
 }
