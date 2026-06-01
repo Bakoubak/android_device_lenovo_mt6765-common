246473b049684f4845218dc23b859ac2a3abfaeb
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-06-01 23:18:01 +0200

    mt6765: don't check kernel version for vintf
    * VTS won't pass on this device, and 4.9 is not supported by AOSP 16 anyway

--- mt6765.mk
+++ mt6765.mk
@@ -7,6 +7,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# VINTF
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
