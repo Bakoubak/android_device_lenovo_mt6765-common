ba57b8fafb51279892bacc5d9fc56ac0679db5db
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-06-16 12:01:53 +0200

    patches: update patches

--- patches/apply.sh
+++ patches/apply.sh
@@ -4,3 +4,4 @@ patch -p1 < ../../device/lenovo/mt6765-common/patches/vendor/lineage/0001-PATCH-
 cd ../..
 cd packages/modules/Connectivity
 patch -p1 < ../../../device/lenovo/mt6765-common/patches/packages/modules/Connectivity/0001-PATCH-Allow-booting-on-4.9-kernels.patch
+patch -p1 < ../../../device/lenovo/mt6765-common/patches/packages/modules/Connectivity/0001-prevent-artificial-requirements-set-by-Google.patch
