065a1d627730cf1c54f21b0f460d299689ea6ed2
Anaël <60509176+Bakoubak@users.noreply.github.com>
2026-06-20 17:11:29 +0200

    mt6765: build f2fs tools explicitly

--- mt6765.mk
+++ mt6765.mk
@@ -314,5 +314,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+# F2FS
+PRODUCT_PACKAGES += \
+    mkfs.f2fs \
+    fsck.f2fs
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/lenovo/mt6765-common/mt6765-common-vendor.mk)
